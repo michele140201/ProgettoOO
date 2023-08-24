840a0a7fa89d2f25d3b4a6a6070c95c1fe0c9926
michele140201 <139127081+michele140201@users.noreply.github.com>
2023-08-24 11:22:56 +0200

    commenti controller avvio

--- src/Controller/ConnectionController.java
+++ src/Controller/ConnectionController.java
@@ -4,11 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Classe che si occupa di gestire la connessione del programma con il database
+ */
 
 public class ConnectionController {
     private final String dbname = "jdbc:postgresql://localhost:5432/Progetto";
     private final String username = "postgres";
     private final String password = "Girafarig20";
+
+    /**
+     * Costruttore del controller che inizializza i driver necessari per connettere il programma al database
+     * @throws ClassNotFoundException
+     */
 
     public ConnectionController() throws ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
